57357203c9c048261a424b4b812a729c6175ecc7
kamile <km662@cam.ac.uk>
2018-09-26 11:36:31 +0100

    Add template view to blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
-# Create your views here.
+from .models import Post
+
+
+class IndexView(generic.ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'latest_post_list'
+
+    def get_queryset(self):
+        """ Return last 5 post (not including those set for future) """
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-pub_date')[:5]
